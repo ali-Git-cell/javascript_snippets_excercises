efe91c9c786f9dbf09a14d165a3177c76338a4ac
Ali Hasham <60347508+ali-Git-cell@users.noreply.github.com>
2023-02-16 23:16:46 +0100

    Update recursive_function2.js

--- recursive_function2.js
+++ recursive_function2.js
@@ -20,8 +20,24 @@ function recursiveFactorial(n) {
 }
 
 
+
+// iterativeFibonacci
+function iterativeFibonacci(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  let previous = 0;
+  let current = 1;
+  for (let i = n; i > 1; i--) {
+    let next = previous + current;
+    previous = current;
+    current = next;
+  }
+  return current;
+}
+
+
 //Fibonacci Series using Recursion
-// function returns the Fibonacci number
     function fibonacci(n) {
     if (n <= 1)
         return n;

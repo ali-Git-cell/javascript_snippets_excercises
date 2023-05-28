2def26815b03f3b4258fcc29affa0c316efb7006
Ali Hasham <60347508+ali-Git-cell@users.noreply.github.com>
2023-05-28 13:51:37 +0200

    Update iterators.js

--- iterators.js
+++ iterators.js
@@ -25,3 +25,43 @@ function returnIterator(arr) {
 // console.log(myIterator()); // -> should log 'b'
 // console.log(myIterator()); // -> should log 'c'
 // console.log(myIterator()); // -> should log 'd'
+
+function nextIterator(array) {
+  let index = 0;
+
+  return {
+    next: function() {
+      if (index < array.length) {
+        return {
+          value: array[index++],
+          done: false
+        };
+      } else {
+        return {
+          done: true
+        };
+      }
+    }
+  };
+}
+
+// Uncomment the below to test
+// const array3 = [1, 2, 3];
+// const iteratorWithNext = nextIterator(array3);
+// console.log(iteratorWithNext.next().value); // -> should log 1
+// console.log(iteratorWithNext.next().value); // -> should log 2
+// console.log(iteratorWithNext.next().value); // -> should log 3
+
+function sumArray(arr) {
+  let total = 0;
+  const iterator = nextIterator(arr);
+  for(let i = 0; i < arr.length; i++){
+    total = total + iterator.next().value
+  }
+  return total;
+}
+
+// Uncomment the below to test
+// Uncomment the lines below to test
+//const array4 = [1, 2, 3, 4];
+//console.log(sumArray(array4)); // -> should log 10
